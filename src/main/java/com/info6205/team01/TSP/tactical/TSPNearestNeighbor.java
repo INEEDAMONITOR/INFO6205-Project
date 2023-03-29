1d65bbba4823270999754a45a18afafc4b786e8e
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-03-28 22:27:38 -0400

    Added TSPNearestNeighbor

---
+++ src/main/java/com/info6205/team01/TSP/tactical/TSPNearestNeighbor.java
@@ -0,0 +1,62 @@
+package com.info6205.team01.TSP.tactical;
+
+import java.util.ArrayList;
+
+public class TSPNearestNeighbor {
+    private int numberOfNodes;
+    private boolean[] visited;
+    private ArrayList<Integer> tour;
+    private double[][] adjacencyMatrix;
+
+    public TSPNearestNeighbor(double[][] adjacencyMatrix) {
+        this.adjacencyMatrix = adjacencyMatrix;
+        numberOfNodes = adjacencyMatrix[0].length;
+        visited = new boolean[numberOfNodes];
+        tour = new ArrayList<>();
+    }
+
+    public void findShortestPath() {
+        visited[0] = true;
+        tour.add(1);
+        int currentPos = 0;
+        int nearestNeighbor = 0;
+        double shortestDistance = 0;
+        boolean minFlag = false;
+
+        for (int i = 1; i < numberOfNodes; i++) {
+            double currentMin = Double.MAX_VALUE;
+            for (int j = 1; j < numberOfNodes; j++) {
+                if (adjacencyMatrix[currentPos][j] != 0 && visited[j] == false) {
+                    if (adjacencyMatrix[currentPos][j] < currentMin) {
+                        currentMin = adjacencyMatrix[currentPos][j];
+                        nearestNeighbor = j;
+                        minFlag = true;
+                    }
+                }
+            }
+
+            if (minFlag == true) {
+                visited[nearestNeighbor] = true;
+                tour.add(nearestNeighbor + 1);
+                minFlag = false;
+                currentPos = nearestNeighbor;
+            }
+            shortestDistance += currentMin;
+        }
+
+        shortestDistance += adjacencyMatrix[currentPos][0];
+
+        System.out.println("Nearest Neighbor Heuristic Algorithm Result: ");
+        System.out.println("Tour: " + tour);
+        System.out.println("Shortest Distance: " + shortestDistance);
+    }
+
+    public static void main(String[] args) {
+        double[][] adjacencyMatrix = {{0, 10, 15, 20},
+                {10, 0, 35, 25},
+                {15, 35, 0, 30},
+                {20, 25, 30, 0}};
+        TSPNearestNeighbor tsp = new TSPNearestNeighbor(adjacencyMatrix);
+        tsp.findShortestPath();
+    }
+}
