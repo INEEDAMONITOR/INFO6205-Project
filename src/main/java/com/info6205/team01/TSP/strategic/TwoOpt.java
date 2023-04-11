e8c24f3bb2e1c1d518ca5a39216054f643c8fd0b
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-04-11 19:40:54 -0400

    Refactor TwoOpt, not runnable

--- src/main/java/com/info6205/team01/TSP/strategic/TwoOpt.java
+++ src/main/java/com/info6205/team01/TSP/strategic/TwoOpt.java
@@ -1,23 +1,40 @@
 package com.info6205.team01.TSP.strategic;
 
+import com.info6205.team01.TSP.Graph.Node;
+import com.info6205.team01.TSP.util.LoadCSVData;
+import com.info6205.team01.TSP.visualization.GraphOperation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class TwoOpt {
-    public double optimize(int[] tour, double[][] distances) {
-        int n = tour.length;
-        int[] newTour = null;
-        double minDistance = calculateDistance(tour, distances);
-        System.out.println("Unchanged distance: " + minDistance);
-        int[] oldTourRef = tour;
+
+    List<Node> tour;
+    int length;
+    List<GraphOperation> gos = new ArrayList<>();
+    double minDistance = 0;
+
+    public TwoOpt(List<Node> nodes) {
+        tour = new ArrayList<>(nodes);
+        minDistance = calculateDistance(tour);
+        length = tour.size();
+
+    }
+    public void optimize() {
+        List<Node> newTour;
         boolean improvement = true;
 
         while (improvement) {
             improvement = false;
 
-            for (int i = 1; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
+            for (int i = 1; i < length-1; i++) {
+                for (int j = i + 1; j < length; j++) {
                     // 2-opt swap
                     newTour = twoOptSwap(tour, i, j);
                     // check new distance
-                    double newDist = calculateDistance(newTour, distances);
+                    double newDist = calculateDistance(newTour);
 
                     if (newDist < minDistance) {
                         // new tour is better, keep it
@@ -28,39 +45,43 @@ public class TwoOpt {
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            oldTourRef[i] = tour[i];
-        }
-
-        return minDistance;
     }
 
     // helper method for performing a 2-opt swap
-    private int[] twoOptSwap(int[] tour, int i, int j) {
-        int[] newTour = new int[tour.length];
+    private List<Node> twoOptSwap(List<Node> tour, int i, int j) {
+        Node[] newTour = new Node[length];
         int index = 0;
         // take all the cities from beginning to i-1
         for (int k = 0; k < i; k++) {
-            newTour[index++] = tour[k];
+            newTour[index++] = tour.get(k);
         }
         // take cities from i to j in reverse order
         for (int k = j; k >= i; k--) {
-            newTour[index++] = tour[k];
+            newTour[index++] = tour.get(k);
         }
         // take cities from j+1 to end
-        for (int k = j + 1; k < tour.length; k++) {
-            newTour[index++] = tour[k];
+        for (int k = j + 1; k < length; k++) {
+            newTour[index++] = tour.get(k);
         }
-        return newTour;
+        List<Node> nList = new ArrayList<>(Arrays.asList(newTour));
+        return nList;
     }
 
     // helper method for calculating the distance of a tour
-    private double calculateDistance(int[] tour, double[][] distances) {
+    private double calculateDistance(List<Node> tour) {
+        Map<String, Integer> IDToIndex = LoadCSVData.data.IDToIndex;
+        double[][] distances = LoadCSVData.data.adjacencyMatrix;
+
         double distance = 0;
-        for (int i = 0; i < tour.length - 1; i++) {
-            distance += distances[tour[i]][tour[i+1]];
+        for (int i = 0; i < length - 1; i++) {
+            int index1 = IDToIndex.get(tour.get(i).getId());
+            int index2 = IDToIndex.get(tour.get(i+1).getId());
+            distance += distances[index1][index2];
         }
-        distance += distances[tour[tour.length-1]][tour[0]];
+        int lastIndex = IDToIndex.get(tour.get(length-1).getId());
+        int firstIndex = IDToIndex.get(tour.get(0).getId());;
+        distance += distances[lastIndex][firstIndex];
+
         return distance;
     }
 }
